ab5428421b3c63346d7df84749fe4c5eb2bc71b5
John Smith <example@stanford.edu>
2013-07-11 18:09:30 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -22,30 +22,45 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://agile-oasis-9492.herokuapp.com/";
+
 
 var assertFileExists = function(infile) {
+
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+
+    if(!fs.exists(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
+//var cheerioHtmlFile = function(htmlfile) {
+ //   return cheerio.load(fs.readFileSync(htmlfile));
+//};
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(isUrl, html, checksfile) {
+
+	if(isUrl == 1)
+	{
+    		$ = cheerio.load(html);
+	}
+	else
+	{
+    		$ = cheerio.load(fs.readFileSync(html));
+	}
+
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +80,36 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+
+	var checkJson;
+	var outJson;
+
+	if(program.url)
+	{
+		rest.get(program.url).on('complete', function(result)
+		{
+			if(result instanceof Error) 
+			{
+        		console.log("Url error %s. Exiting.", result.message);
+        		process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    		}
+
+    		checkJson 	= checkHtmlFile(1, result, program.checks);
+    		outJson 	= JSON.stringify(checkJson, null, 4);
+    		console.log(outJson);
+		});
+
+	}
+	else
+	{
+    	checkJson	= checkHtmlFile(0, program.file, program.checks);
+    	outJson 	= JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+	}
+} 
+else 
+{
     exports.checkHtmlFile = checkHtmlFile;
 }
